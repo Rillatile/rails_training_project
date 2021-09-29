573d66c5fd084b4b93842a461953995fd67dc30f
Роман Малинков <roman.malinkov@list.ru>
2021-09-29 23:25:55 +0700

    seeds improvement

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,21 @@
 
 author_seeds = Rails.root.join('db', 'seeds', 'authors.yml')
 authors_config = YAML.load_file(author_seeds)
-Author.create!(authors_config)
+
+authors_config.each { |attributes| Author.find_or_create_by(attributes) }
 
 book_seeds = Rails.root.join('db', 'seeds', 'books.yml')
 books_config = YAML.load_file(book_seeds)
-Book.create!(books_config)
+
+books_config.each do |attributes|
+  Book.find_or_create_by(attributes) do |book|
+    case book.title
+    when 'Капитанская дочка'
+      book.author = Author.find_by(surname: 'Пушкин')
+    when 'Будущее', 'Метро 2033'
+      book.author = Author.find_by(surname: 'Глуховский')
+    when 'Ночной дозор', 'Дневной дозор'
+      book.author = Author.find_by(surname: 'Лукьяненко')
+    end
+  end
+end
